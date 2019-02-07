8bd06b4f48097edb3b515375c256352d611139fd
GaneshSPatil <ganeshpl@thoughtworks.com>
2019-02-07 11:40:50 +0530

    Add some tests to increase coverage

--- test/unit_test.js
+++ test/unit_test.js
@@ -1,8 +1,13 @@
 const assert = require('assert');
 const add = require('../util').add;
+const sub = require('../util').subtract;
 
 describe('Unit', function () {
   it('should check if 1 is equal is true', function () {
     assert.equal(add(2, 3), 5);
+  });
+
+  it('should check if 1 is equal is true', function () {
+    assert.equal(sub(2, 3), -1);
   });
 });
