2b6db57ea3ff22ee1e96992393a5532debc911f8
Mahesh Sovani <44019471+maheshsovani@users.noreply.github.com>
2019-02-09 09:22:57 +0530

    removed error from the uniot test.  Please enter the commit message for your changes. Lines starting

--- test/unit_test.js
+++ test/unit_test.js
@@ -2,12 +2,12 @@ const assert = require('assert');
 const add = require('../util').add;
 const sub = require('../util').subtract;
 
-describe('Unit', function () {
-  it('should check if 1 is equal is true', function () {
-    assert.equal(add(2, 3), 1);
+describe('Unit', function() {
+  it('should check if 1 is equal is true', function() {
+    assert.equal(add(2, 3), 5);
   });
 
-  it('should check if 1 is equal is true', function () {
+  it('should check if 1 is equal is true', function() {
     assert.equal(sub(2, 3), -1);
   });
 });
